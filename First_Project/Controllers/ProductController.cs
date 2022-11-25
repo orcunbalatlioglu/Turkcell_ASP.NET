68c5e7596632aa613be87769992ab47c0bd37c68
Orçun Balatlıoğlu <orcun521@gmail.com>
2022-11-26 02:19:55 +0300

    Product ve ProductPage EF Core Database First Yaklaşımı

--- First_Project/Controllers/ProductController.cs
+++ First_Project/Controllers/ProductController.cs
@@ -3,20 +3,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace First_Project.Controllers
 {
+    
     public class ProductController : Controller
     {
+        private AppDbContext _context;
         private readonly ProductRepository _productRepository;
-        public ProductController() { 
+
+        public ProductController(AppDbContext context) { 
             _productRepository = new ProductRepository();
+            _context = context;
+            if (!_context.Products.Any()) { 
+                _context.Products.Add(new Product() { Name = "Kalem1", Price = 100, Stock = 3 ,Color="Red",Width=10,Height=20});
+                _context.Products.Add(new Product() { Name = "Kalem2", Price = 200, Stock = 4 ,Color="Blue",Width=11});
+                _context.Products.Add(new Product() { Name = "Kalem3", Price = 300, Stock = 5 ,Color="Yellow"});
+                _context.Products.Add(new Product() { Name = "Kalem4", Price = 400, Stock = 6 });
+                
+                _context.SaveChanges();
+            }
         }
         public IActionResult ProductPage()
         {
-            var products = _productRepository.GetAll();
+            var products = _context.Products.ToList();
             return View(products);
         }
         public IActionResult Remove(int id)
         {
-            _productRepository.Remove(id);
+            var _product = _context.Products.Find(id);
+            _context.Products.Remove(_product);
+            _context.SaveChanges();
             return RedirectToAction("ProductPage");
         }
         public IActionResult Add()
--- First_Project/Program.cs
+++ First_Project/Program.cs
@@ -1,8 +1,14 @@
+using First_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
